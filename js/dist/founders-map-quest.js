17f23f6e5c3f0e3d3d099df3b26e61e250a91ce1
mcarmenjimenezcampos <mcarmen.jimenez.campos@onalytica.com>
2015-08-13 11:47:24 +0100

    generated doc and dist js file

--- js/dist/founders-map-quest.js
+++ js/dist/founders-map-quest.js
@@ -17,7 +17,7 @@ app.ColumnModel = Backbone.Model.extend(/** @lends ColumnModel.prototype */ {
 		}
 	},
 	/** 
-     * Get default value for a column
+     * Gets default value for a column
      * @returns {Object} An object with the defaults values to set
      */
 	defaults: function(){
@@ -26,7 +26,7 @@ app.ColumnModel = Backbone.Model.extend(/** @lends ColumnModel.prototype */ {
 		};
 	},
 	/** 
-     * Get name property value
+     * Gets name property value
      * @returns {string} The name of the column
      */
 	getName: function(){
@@ -44,7 +44,7 @@ var app = app || {};
  */
 app.ErrorModel = Backbone.Model.extend(/** @lends ErrorModel.prototype */ {
 	/** 
-     * Get default value for an error
+     * Gets default value for an error
      * @returns {Object} An object with the defaults values to set
      */
 	defaults: function(){
@@ -69,16 +69,17 @@ app.RowModel = Backbone.Model.extend(/** @lends RowModel.prototype */ {
      */
 	 initialize: function(attributes){
 	 	var me = this;
-	 	me.set('hide', false);
+	 	me.set('hiddenMarker', false);
+	 	me.set('hidden', false);
 	 	me.set(attributes);
 	 },
 	 /** 
-     * Set value of hide property to !hide, to show or hide the marker on the map
+     * Sets value of hide property to !hide, to show or hide the marker on the map
      */
-	 toggle: function(){
+	 toggleMarker: function(){
 	 	var me = this;
-	 	me.set('hide', !me.get('hide'));
-	 	if (me.get('hide')){
+	 	me.set('hiddenMarker', !me.get('hiddenMarker'));
+	 	if (me.get('hiddenMarker')){
 	 		me.hideMarker();
 	 	}
 	 	else{
@@ -86,14 +87,14 @@ app.RowModel = Backbone.Model.extend(/** @lends RowModel.prototype */ {
 	 	}
 	 },
 	 /** 
-     * Get if the row is hidden from the map or not
-     * @returns {boolean} True if the row is hidden and false if not
+     * Gets if the row is hidden from the map or not
+     * @returns {boolean} True if the row marker is hidden and false if not
      */
-	 isHidden: function(){
-	 	return this.get('hide');
+	 isMarkerHidden: function(){
+	 	return this.get('hiddenMarker');
 	 },
 	 /** 
-     * Hide the marker from the map, removing value of the map property of the marker
+     * Hides the marker from the map, removing value of the map property of the marker
      */
 	 hideMarker: function(){
 	 	var me = this,
@@ -101,12 +102,44 @@ app.RowModel = Backbone.Model.extend(/** @lends RowModel.prototype */ {
 	 	marker.setMap(null);
 	 },
 	 /** 
-     * Set map property of the marker, to make it visible again
+     * Sets map property of the marker, to make it visible again
      */
 	 showMarker: function(){
 	 	var me = this,
 	 		marker = me.get('googleMarker');
 	 	marker.setMap(me.get('googleMap'));
+	 },
+	 /** 
+     * Sets the row as hidden and hides the marker
+     */
+	 hide: function(){
+	 	var me = this;
+	 	me.set('hidden', true);
+	 	me.hideMarker();
+	 },
+	 /** 
+     * Sets the row as shown and shows the marker if it is not hidden
+     */
+	 show: function(){
+	 	var me = this;
+	 	me.set('hidden', false);
+	 	if(!me.get('hiddenMarker')){
+	 		me.showMarker();
+	 	}
+	 },
+	 /** 
+     * Checks if the row is hidden or not
+     * @returns {boolean} True if the row is hidden and false if not
+     */
+	 isHidden: function(){
+	 	return this.get('hidden');
+	 },
+	 /** 
+     * Checks if the row should be hidden or not
+     * @returns {boolean} True if the row should be hidden and false if not
+     */
+	 shouldBeHidden: function(column, value){
+	 	return this.get(column).toUpperCase().indexOf(value.toUpperCase()) === -1;
 	 },
 	 /** 
      * Initialise googleMarker property
@@ -162,49 +195,51 @@ app.RowCollection = Backbone.Collection.extend(/** @lends RowCollection.prototyp
      */
 	model: app.RowModel,
 	/** 
-     * @property {Array} originalArray The array of data set the first time to create the collection
-     * (needed to come back from filtering)
-     */
-	originalRowArray: undefined,
-	/** 
-     * Sort the rows in the collection by a specific column
+     * Sets the comparator property to the column that it is going to be used to sort the elements
      * @param {string} column Name of the column (property) to sort the rows by
      */
-	sortBy: function(column){
-		var me = this,
-			sortedArray = _.sortBy(me.originalRowArray, column);
-		me.reset(sortedArray);
+	setComparator: function(column){
+		this.comparator = column;
 	},
 	/** 
-     * Get a subset of the rows in the collection filtered by a specific value of a column
+     * Checks for each row if it should be shown or hidden depending on the attribute value
      * @param {string} column Name of the column (property) to filter
      * @param {string} value Value to filter by
      */
 	filter: function(column, value){
-		var me = this,
-			filteredArray = _.filter(me.originalRowArray, function(row){
-				return row[column].toUpperCase().indexOf(value.toUpperCase()) !== -1;
-			});
-		me.reset(filteredArray);
-	},
-	/** 
-     * Set data into the collection. It creates a copy of this array of data in case it is needed
-     * for coming back from filtering.
-     * @param {Array} rowArray Array of objects that contains the data for the rows
-     */
-	setData: function(rowArray){
 		var me = this;
-		me.originalRowArray = rowArray;
-		me.each(function(row){
-			row.hideMarker();
-		});
-		me.reset(rowArray);
+		me.filterColumn = column;
+		me.filterValue = value;
+		me.each(me.applyFilterToRow, me);
 	},
 	/** 
-     * Remove any applied filter. It sets back the orginal array of data saved.
+     * Checks if the specific row should be hidden and update it taking that into account
+     * @param {Object} row Row model to check
+     */
+	applyFilterToRow: function(row){
+		var me = this,
+			column = me.filterColumn,
+			value = me.filterValue;
+		if (!row.shouldBeHidden(column, value)){
+			row.show();
+		}
+		else{
+			row.hide();
+		}
+	},
+	/** 
+     * Removes any applied filter. It sets back the orginal options.
      */
 	removeFilter: function(){
-		this.reset(this.originalRowArray);
+		var me = this;
+		me.each(me.showRow);
+	},
+	/** 
+     * Shows the specific row
+     * @param {Object} row Row model to show
+     */
+	showRow: function(row){
+		row.show();
 	}
 }); 
 /*global Backbone */
@@ -226,7 +261,7 @@ app.ErrorView = Backbone.View.extend(/** @lends ErrorView.prototype */{
       this.template = _.template($('#error-template').html());
     },
     /** 
-     * Render the error. Create the html code for rendering the error based on the template.
+     * Renders the error. Create the html code for rendering the error based on the template.
      * @returns {Object} It returns itself
      */
     render: function(){
@@ -286,6 +321,7 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
 
         me.listenTo(app.Rows, 'add', me.addRow);
         me.listenTo(app.Rows, 'reset', me.addAllRows);
+        me.listenTo(app.Rows, 'sort', me.addAllRows);
 
         me.$columnSelectionRow.hide();
         me.$resultRow.hide();
@@ -300,7 +336,7 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
         me.$fileChooser.click();
     },
     /** 
-     * Get the selected file to process it.
+     * Gets the selected file to process it.
      */
     processFile: function() {
         var fileName = this.$fileChooser.val(),
@@ -313,37 +349,69 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
         }
     },
     /** 
-     * Set collections value from the data in the file.
+     * Checks if the file type is correct. If it is not correct it will show an error.
+     * @returns {boolean} True if there is an error or false if not
+     */
+    checkFileType: function(fileName){
+        var me = this,
+            error = false;
+        if (fileName.substring(fileName.length-4) !== '.csv'){
+            me.showTypeError();
+            error = true;
+        }
+        return error;
+    },
+    /** 
+     * Checks if the file size is greater than. If it is not it will show an error.
+     * @returns {boolean} True if there is an error or false if not
+     */
+    checkFileSize: function(fileSize){
+        var me = this,
+            error = false;
+        if (fileSize === 0){
+            me.showNoDataError();
+            error = true;
+        }
+        return error;
+    },
+    /** 
+     * Parses the file using Papa.parse. In case there are errors, they will be displayed. 
+     * If not it will empty the view and reset the columns and rows collections.
+     * @param {Object} file File to parse and extract info
+     */
+    parseFile: function(file){
+        var me = this;
+        Papa.parse(file, {
+            header: true,
+            complete: function(results) {
+                if (results.errors.length > 0){
+                    me.showParsingError();
+                }
+                else{
+                    me.$table.html('');
+                    app.Columns.reset(results.meta.fields);
+                    app.Rows.reset(results.data);
+                    me.showSelectOptions();
+                }
+            }
+        });
+    },
+    /** 
+     * Sets collections value from the data in the file.
      * @param {Object} file File content
      */
     setDataFromFile: function(file){
-        var me = this;
-        if (file.type !== 'text/csv'){
-            me.showTypeError();
-        }
-        if (file.size === 0){
-            me.showNoDataError();
-        }
-        if (file.type === 'text/csv' && file.size > 0){
-            Papa.parse(file, {
-                header: true,
-                complete: function(results) {
-                    if (results.errors.length > 0){
-                        me.showParsingError();
-                    }
-                    else{
-                        me.$table.html('');
-                        app.Columns.reset(results.meta.fields);
-                        app.Rows.setData(results.data);
-                        me.showSelectOptions();
-                    }
-                }
-            });
+        var me = this,
+            error = false;
+        error = me.checkFileType(file.name) || me.checkFileSize(file.size);
+        if (!error){
+            app.Rows.reset();
+            me.parseFile(file);
         }
      },
     /** 
-     * Hide file chooser row and show dropdowns to select latitude, longitude and marker columns. 
-     * Set dropdown options to the list of columns
+     * Hides file chooser row and show dropdowns to select latitude, longitude and marker columns. 
+     * Sets dropdown options to the list of columns
      */
     showSelectOptions: function(){
         var me = this;
@@ -355,7 +423,7 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
         me.$errorRow.html('');
     },
     /** 
-     * Set the dropdown list options to the columns list
+     * Sets the dropdown list options to the columns list
      * @param {string} elementId DOM element id to select the dropdown
      * @param {boolean} isOptional It's true if the value to select could have 
      *  no value, so empty string is added to the options of the dropdownlist
@@ -375,7 +443,7 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
         dropdownList[0].options[0].selected = true;
     },
     /** 
-     * Show the result row and hide the dropdown row. Initialise the map when the section is
+     * Shows the result row and hide the dropdown row. Initialise the map when the section is
      * visible, if not a gray rectangle is displayed.
      * Latitude and Longitud value should be different, because to get a good position they cannot
      * reference the same value
@@ -402,7 +470,7 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
         }
     },
     /** 
-     * Create the markers in the map.
+     * Creates the markers in the map.
      * @param {string} latitudeColumn Column with the latitude value
      * @param {string} longitudeColumn Column with the longitude value
      * @param {string} markerColumn Column with the marker value
@@ -414,7 +482,7 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
         });
     },
     /** 
-     * Initialise the map values, and add it to the element in the DOM
+     * Initialises the map values, and add it to the element in the DOM
      * @param {Object} mapCenter A point in the map to center it
      */
     initialiseMap : function(mapCenter) {
@@ -427,7 +495,7 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
         this.map = new google.maps.Map(mapCanvas, mapOptions);
     },
     /** 
-     * Get the point where the map is going to be centered from the first row in the collection.
+     * Gets the point where the map is going to be centered from the first row in the collection.
      * @param {string} latitude Name of the property which contains the latitude value
      * @param {string} longitude Name of the property which contains the longitude value
      * @returns {Object} A point where the map is going to be centered
@@ -450,7 +518,7 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
         app.Rows.each(this.addRow, this);
     },
     /** 
-     * Create a new app.RowView for a row and add it to the table.
+     * Creates a new app.RowView for a row and add it to the table.
      * @param {Object} row row model to display
      */
     addRow: function(row) {
@@ -458,36 +526,34 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
         this.$table.append(view.render().el);
     },
     /** 
-     * Retrieve from dropdown and text field the values to filter the rows in the table.
+     * Retrieves from dropdown and text field the values to filter the rows in the table.
      */
     filterRows: function(){
         var me = this,
             filterColumn = me.$('#dropdown-filter').val(),
             filterValue = me.$('#filter-value').val();
         app.Rows.filter(filterColumn, filterValue);
-        me.createMapMarkers(me.latitudeColumn, me.longitudeColumn, me.markerColumn, me.map);
     },
     /** 
-     * Sort rows in the table depending on the column selected in the drop down list.
+     * Sorts rows in the table depending on the column selected in the drop down list.
      */
     sortRows: function(){
         var me = this,
             sortColumn = me.$('#dropdown-sort').val();
-        app.Rows.sortBy(sortColumn);
-        me.createMapMarkers(me.latitudeColumn, me.longitudeColumn, me.markerColumn, me.map);
+        app.Rows.setComparator(sortColumn);
+        app.Rows.sort();
     },
     /** 
-     * Remove the applied filter to the table.
+     * Removes the applied filter to the table.
      */
     removeFilter: function(){
         var me = this;
         me.$('#dropdown-filter').val('');
         me.$('#filter-value').val('');
         app.Rows.removeFilter();
-        me.createMapMarkers(me.latitudeColumn, me.longitudeColumn, me.markerColumn, me.map);
     },
     /** 
-     * Move back to the initial step.
+     * Moves back to the initial step.
      */
     beginAgain: function (argument) {
         var me = this;
@@ -496,7 +562,7 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
         me.$selectFileRow.show();
     },
     /** 
-     * Move back to the column selection for latitude, longitude and marker.
+     * Moves back to the column selection for latitude, longitude and marker.
      */
     goBack: function(){
         var me = this;
@@ -505,7 +571,7 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
         me.$columnSelectionRow.show();
     },
     /** 
-     * Create error message when type of file is not correct
+     * Creates error message when type of file is not correct
      */
     showTypeError: function(){
         var error = new app.ErrorModel({message: 'Looks like it\'s the wrong file format, make sure it\'s a .csv'}),
@@ -514,7 +580,7 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
         me.$errorRow.append(errorView.render().el);
     },
     /** 
-     * Create error message when file is empty
+     * Creates error message when file is empty
      */
     showNoDataError: function(){
         var error = new app.ErrorModel({message: 'Looks like the file is empty, please upload another file'}),
@@ -523,7 +589,7 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
         me.$errorRow.append(errorView.render().el);
     },
     /** 
-     * Create error message when file is not parsed correctly
+     * Creates error message when file is not parsed correctly
      */
     showParsingError: function(){
         var error = new app.ErrorModel({message: 'Looks like there\'s a problem processing the file, please try with another file'}),
@@ -532,7 +598,7 @@ app.FileView = Backbone.View.extend(/** @lends FileView.prototype */{
         me.$errorRow.append(errorView.render().el);
     },
     /** 
-     * Create error message when latitude and longitude columns are the same
+     * Creates error message when latitude and longitude columns are the same
      */
     showEqualLatitudeLongitudeValues: function(){
         var error = new app.ErrorModel({message: 'Looks like you have selected the same column for latitude and longitude'}),
@@ -556,17 +622,20 @@ app.RowView = Backbone.View.extend(/** @lends RowView.prototype */{
      * @property {Object} events Associates DOM events with a function to process them
      */
     events: {
-      "click #toggle"   : "toggleMarker",
+      "click #toggle"   : "toggleMarker"
     },
     /** 
      * @contructs RowView object.
      * Initialize the related template in the DOM
+     * @listens RowModel:change:hidden
      */
     initialize: function(){
-      this.template = _.template($('#row-template').html());
+      var me = this;
+      me.template = _.template($('#row-template').html());
+      me.listenTo(me.model, 'change:hidden', me.toggleRow);
     },
     /** 
-     * Render the row. Create the html code for rendering the row. It manages if the content of the cell
+     * Renders the row. Create the html code for rendering the row. It manages if the content of the cell
      * is a link or an image
      * @returns {Object} It returns itself
      */
@@ -577,7 +646,7 @@ app.RowView = Backbone.View.extend(/** @lends RowView.prototype */{
             var dataCell,
                 isLink,
                 extension;
-            if(key !== 'hide'){
+            if(key !== 'hiddenMarker' && key !== 'hidden' && key !== 'googleMap' && key !== 'googleMarker'){
               me.$el.find('#data-table')
               .append($('<tr>')
                 .append($('<td>')
@@ -611,7 +680,19 @@ app.RowView = Backbone.View.extend(/** @lends RowView.prototype */{
      * Toggle if the row should be displayed or not in the map.
      */
     toggleMarker: function(){
-        this.model.toggle();
+        this.model.toggleMarker();
+    },
+    /** 
+     * Depending if the row is hidden or not, the DOM element is going to be hidden or shown.
+     */
+    toggleRow: function(){
+      var me = this;
+      if (me.model.isHidden()){
+        me.$el.hide();
+      }
+      else{
+        me.$el.show();
+      }
     }
 });
 /*global Backbone */
